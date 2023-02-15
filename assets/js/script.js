8baacc4d7b07dfd9e49aec7c69f4deec167e4451
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-02-15 15:51:17 -0700

    udpate var questions, add hide class and HTML content

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var questions = [
+  {
+    title: "How do you indicate a comment in JavaScript?",
+    choices: ["?", "comment:", "//", "<!--"],
+    answer: "//",
+  },
+  {
+    title: "True or False: Variables in JavaScript are case sensitive.",
+    choices: ["True", "False"],
+    answer: "True",
+  },
+  {
+    title: "What symbols indicate an object?",
+    choices: ["{}", "[]", "()", "<>"],
+    answer: "{}",
+  },
+];
