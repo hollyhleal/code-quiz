e88cc296d32e69d21fc40338b75bf409ad059268
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-02-15 20:05:07 -0700

    add functions for finishGame and highScores

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,8 @@ var questions = [
 var directions = document.querySelector(".start");
 var clickStart = document.querySelector("#startButton");
 var gameInPlay = document.querySelector(".game");
+var finalScore = document.querySelector(".finalScore");
+var highScores = document.querySelector(".highScores");
 
 function startGame() {
   directions.classList.add("hide");
@@ -32,9 +34,25 @@ startButton.addEventListener("click", startGame);
 
 //   //need to reference the global variable questions
 //   //potentially use a for loop to iterate through questions?
-//   gameInPlay = "";
+var launchQuestions = "";
 
-//   for (var i = 0; i < questions.length; i++) {
-//     questions[i].textContent = "";
-//   }
-// });
+for (var i = 0; i < questions.length; i++) {
+  questions[i].textContent = "";
+}
+
+//when for loop concludes, call finishGame() function
+function finishGame() {
+  gameInPlay.classList.add("hide");
+  finalScore.classList.remove("hide");
+}
+
+//when "View High Scores" is clicked, highScores() function will run
+function highScores() {
+  directions.classList.add("hide");
+  gameInPlay.classList.add("hide");
+  finalScore.classList.add("hide");
+  highScores.classList.remove("hide");
+}
+
+//add event Listener for "View High Scores"
+
