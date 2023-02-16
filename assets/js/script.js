2b03e9e6d33d9284a2118a7710769277113598c5
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-02-15 17:18:15 -0700

    update to hide/show function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//VARIABLE DECLARATIONS
 var questions = [
   {
     title: "How do you indicate a comment in JavaScript?",
@@ -15,3 +16,28 @@ var questions = [
     answer: "{}",
   },
 ];
+
+//storing references to elements in the DOM
+var startGame = document.querySelector("#startButton");
+var gameInPlay = document.querySelector(".game");
+
+var page = "show";
+
+//add event listener to start button to start the game
+// startGame.addEventListener("click", function () {
+//   if (page === "show") {
+//     page = "hide";
+//     page.setAttribute("class", "hide");
+//   }
+// });
+
+// startGame.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   //need to reference the global variable questions
+//   //potentially use a for loop to iterate through questions?
+//   gameInPlay = "";
+
+//   for (var i = 0; i < questions.length; i++) {
+//     questions[i].textContent = "";
+//   }
+// });
